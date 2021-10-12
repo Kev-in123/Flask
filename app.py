f9452043985fa5e48a2eafbab17e901a6480c8c6
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-12 09:45:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def awards():
 def contact():
     return render_template("contact.html")
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
 if __name__ == "__main__":
-    run()
+    app.run()
+
